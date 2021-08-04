9d8f19f8aa67f6d1516c6dcdd238f68e56d0ed66
root <root@adama>
2021-08-04 22:27:08 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='netbox_fusioninventory_plugin',
+    version='0.1',
+    description='A Plugin for import devices from fusion inventory agent',
+    url='https://gitlab.com/',
+    author='Michaël Ricart',
+    license='BSD License',
+    install_requires=[
+        'bs4',
+        'lxml',
+        ],
+    packages=find_packages(),
+    include_package_data=True,
+)
+

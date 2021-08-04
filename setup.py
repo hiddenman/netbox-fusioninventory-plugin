8c7c4751881ef3c8a3e62fbd0710a0409adfdb42
root <root@adama>
2021-08-04 23:12:16 +0200

    fix url in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='netbox_fusioninventory_plugin',
     version='0.1',
     description='A Plugin for import devices from fusion inventory agent',
-    url='https://gitlab.com/',
+    url='https://gitlab.com/Milka64/netbox-fusioninventory-plugin',
     author='Michael Ricart',
     license='BSD License',
     install_requires=[

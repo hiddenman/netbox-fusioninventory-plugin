e5738f3c7368cd6178380b6bcbbaca03f9625053
Andrew Kornilov <akornilov@gmail.com>
2021-11-30 13:00:23 +0300

    Use unicode_slugify from Mozilla. Otherwise we get duplicate key errors on Unicode names and django slugify()

--- netbox_fusioninventory_plugin/utils.py
+++ netbox_fusioninventory_plugin/utils.py
@@ -3,7 +3,7 @@ from dcim.models import *
 from ipam.models import IPAddress
 from tenancy.models import *
 from pprint import *
-from django.utils.text import slugify
+from slugify import slugify, SLUG_OK
 from django.conf import settings
 from extras.models import Tag
 
@@ -246,44 +246,44 @@ def created_or_update_device(device_dict, items_array):
                 if key == "device_role":
                     device_dict[key] = DeviceRole.objects.get_or_create(
                         name=device_dict[key],
-                        slug=slugify(device_dict[key]),
+                        slug=slugify(device_dict[key], only_ascii=True),
                     )[0]
                 elif key == "manufacturer":
                     device_dict[key] = Manufacturer.objects.get_or_create(
                         name=device_dict[key],
-                        slug=slugify(device_dict[key]),
+                        slug=slugify(device_dict[key], only_ascii=True),
                     )[0]
                 elif key == "tenant":
                     device_dict[key] = Tenant.objects.get_or_create(
                         name=device_dict[key],
-                        slug=slugify(device_dict[key]),
+                        slug=slugify(device_dict[key], only_ascii=True),
                     )[0]
                 elif key == "device_type":
                     device_dict[key] = DeviceType.objects.get_or_create(
                         model=device_dict[key],
-                        slug=slugify(device_dict[key]),
+                        slug=slugify(device_dict[key], only_ascii=True),
                         manufacturer=device_dict["manufacturer"]
                     )[0]
                     del device_dict["manufacturer"]
                 elif key == "platform":
                     device_dict[key] = Platform.objects.get_or_create(
                         name=device_dict[key],
-                        slug=slugify(device_dict[key]),
+                        slug=slugify(device_dict[key], only_ascii=True),
                     )[0]
                 elif key == "site":
                     device_dict[key] = Site.objects.get_or_create(
                         name=device_dict[key],
-                        slug=slugify(device_dict[key]),
+                        slug=slugify(device_dict[key], only_ascii=True),
                     )[0]
                 elif key == "location":
                     device_dict[key] = Location.objects.get_or_create(
                         name=device_dict[key],
-                        slug=slugify(device_dict[key]),
+                        slug=slugify(device_dict[key], only_ascii=True),
                     )[0]
                 elif key == "rack":
                     device_dict[key] = Rack.objects.get_or_create(
                         name=device_dict[key],
-                        slug=slugify(device_dict[key]),
+                        slug=slugify(device_dict[key], only_ascii=True),
                     )[0]
         else:
             del device_dict[key]
@@ -797,7 +797,7 @@ def created_or_update_device(device_dict, items_array):
                                     try:
                                         item[k] = Manufacturer.objects.get_or_create(
                                             name=item[k],
-                                            slug=slugify(item[k]),
+                                            slug=slugify(item[k], only_ascii=True),
                                         )[0]
                                     except Exception as e:
                                         logger.error(e)
@@ -1685,7 +1685,7 @@ def created_or_update_device(device_dict, items_array):
                                     try:
                                         item[k] = Manufacturer.objects.get_or_create(
                                             name=item[k],
-                                            slug=slugify(item[k]),
+                                            slug=slugify(item[k], only_ascii=True),
                                         )[0]
                                     except Exception as e:
                                         logger.error(e)
@@ -1854,7 +1854,7 @@ def soup_to_dict(soup):
                         obj_type + ".objects.filter(id=" + value + ")[0]")
                 else:
                     device[k] = eval(obj_type + ".objects.get_or_create(name='" +
-                                     value + "', slug=slugify('" + value + "'))")[0]
+                                     value + "', slug=slugify('" + value + "', only_ascii=True))")[0]
             elif value_type == "lazy":
                 # Leave lazy for the second loop
                 # FIXME: Too simple, we should support nested variables.
--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     install_requires=[
         'beautifulsoup4',
         'lxml',
+        'unicode-slugify',
         ],
     packages=find_packages(),
     include_package_data=True,
